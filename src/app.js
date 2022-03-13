c280775f5e3b87c632b9f7eff60cbe3c15a7ccaf
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-12 19:07:56 -0700

    Collapsible buttons recomendations section

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
+  var arrow = document.querySelector('#coll-btn');
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      content.style.display = "none";
+      content.style.display = "none"; 
+      arrow.classList.toggle('fa-rotate-180');
     } else {
       content.style.display = "block";
+      arrow.classList.toggle('fa-rotate-180');
     }
   });
 }
