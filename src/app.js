9a961ffac16f411ec9819def81a732beeab8151d
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-13 21:33:47 -0700

    Landpage structure v1.0 done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  var arrow = document.querySelector('#coll-btn');
+  var arrow = document.querySelectorAll('#coll-btn');
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
